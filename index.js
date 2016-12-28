9fd01acbd0471cdc7c8c0d63b794ab01faf2fcb9
Brian Stone <brians@zillowgroup.com>
2016-12-28 16:47:35 -0500

    Load configuration from .rc file.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var gutil = require('gulp-util');
 var through = require('through2');
 var webpackStatsDuplicates = require('webpack-stats-duplicates');
+var loadConfig = webpackStatsDuplicates.loadConfig;
 var findDuplicates = webpackStatsDuplicates.findDuplicates;
 var printDuplicates = webpackStatsDuplicates.printDuplicates;
 var PLUGIN_NAME = 'gulp-webpack-stats-duplicates';
@@ -16,6 +17,17 @@ module.exports = function (opts) {
         if (file.isStream()) {
             return this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
         }
+
+        var that = this;
+        loadConfig(options.config, function (error, config) {
+            if (error) {
+                that.emit('error', new gutil.PluginError(PLUGIN_NAME, error));
+                return;
+            }
+
+            // Merge config options with the gulp plugin options
+            options = Object.assign({}, config, options);
+        });
 
         var json = JSON.parse(file.contents.toString(enc));
         var duplicates = findDuplicates(json, options);
