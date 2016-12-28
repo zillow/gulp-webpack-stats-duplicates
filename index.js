354c1e8360dbff01c209e1bb51e6855aff36cb80
Brian Stone <brians@zillowgroup.com>
2016-12-28 16:26:09 -0500

    Initial files

---
+++ index.js
@@ -0,0 +1,31 @@
+var gutil = require('gulp-util');
+var through = require('through2');
+var webpackStatsDuplicates = require('webpack-stats-duplicates');
+var findDuplicates = webpackStatsDuplicates.findDuplicates;
+var printDuplicates = webpackStatsDuplicates.printDuplicates;
+var PLUGIN_NAME = 'gulp-webpack-stats-duplicates';
+
+module.exports = function (opts) {
+    var options = opts || {};
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            return cb(null, file);
+        }
+
+        if (file.isStream()) {
+            return this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+        }
+
+        var json = JSON.parse(file.contents.toString(enc));
+        var duplicates = findDuplicates(json, options);
+
+        if (duplicates.length) {
+            printDuplicates(duplicates);
+            return this.emit('error', new gutil.PluginError(
+                PLUGIN_NAME, 'Duplicate modules found in file: ' + file.path));
+        }
+
+        cb(null, file);
+    });
+};
